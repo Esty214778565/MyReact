e4776a37152e83ae80dbe5f35f56210d473be77d
Esty214778565 <8578397@gmail.com>
2025-02-04 00:36:28 +0200

    finall project

--- RecipeBook/src/Layout.tsx
+++ RecipeBook/src/Layout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router";
-import { UserContext, } from "./components/HomePage";
 import NavBar from "./components/NavBar";
-import { useContext } from "react";
 
 const Layout = () => {
-    const user = useContext(UserContext);
     return (<>
-        <UserContext.Provider value={user}>
-            <NavBar />
-        </UserContext.Provider>
+        <NavBar />
         <Outlet />
     </>)
 }
